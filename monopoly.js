0b2a932dc40c3178d77a619ca9c356e32898b4d6
unknown <Doodle23@yahoo.com>
2021-09-05 23:27:04 +0900

    added for loop

--- monopoly.js
+++ monopoly.js
@@ -4,12 +4,20 @@ let DiceRolled = [];
 
 
 let playerLocation = [];
+let space = document.getElementsByClassName("space");
+let prices = document.getElementsByClassName("price")
+//let names = document.getElementsByClassName("name")
 
 
-console.log(playerLocation)
+function printboardspaces(){
+    var length = document.getElementsByClassName("name").length;
 
+    for(var i = 0; i < length; i++){
+         console.log(document.getElementsByClassName("name")[i].innerHTML);
+    }
+}
 
-
+printboardspaces()
 
 PlayerRoll = 0; 
 activePlayer = 0; 
@@ -29,10 +37,10 @@ playerLocation.push(DiceRolled);
 
 let TotalRoll = playerLocation.reduce(
     (accumulator, currentValue) => accumulator + currentValue); 
-    
-   
-document.querySelector('#spaceCount-' + activePlayer).innerHTML =TotalRoll;
+  
 
+    
+document.querySelector('#spaceCount-' + activePlayer).innerHTML = TotalRoll
 
 
 
@@ -49,13 +57,8 @@ document.querySelector('#spaceCount-' + activePlayer).innerHTML =TotalRoll;
 
 
 
+
 space = document.querySelectorAll('.space'); 
-
-
-
-
-
-
 
 
 const spacesCollection = document.querySelectorAll('.space');
@@ -63,9 +66,12 @@ let spacesArr = Array.from(spacesCollection);
 
 spacesArr.forEach(function () {
 
+    
+   
+    
+
+
 })
-
-
 
 
 
