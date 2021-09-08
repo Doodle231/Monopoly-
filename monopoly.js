ad9eb68ee4b8d15ed852abba8a22cdd0d7be5cb1
Dan Royals <59464239+Doodle231@users.noreply.github.com>
2021-09-08 16:39:15 +0900

    Add files via upload

--- monopoly.js
+++ monopoly.js
@@ -4,13 +4,35 @@ let DiceRolled = [];
 
 
 let playerLocation = [];
+//console.log(playerLocation)
+let space = document.getElementsByClassName("space");
+let prices = document.getElementsByClassName("price")
+let names = document.getElementsByClassName("name")
 
 
-console.log(playerLocation)
+
+      
+
+    
+
+let spacenamesUnordered= Array.from(names);
+    const index = [9,8,7,6,5,4,3,2,1,0,19,18,17,16,15,14,13,12,11,
+        10,29,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38,39]
+    const spaceNames = index.map(i => spacenamesUnordered[i])
+    
+
+//class boardspaces {
+   // constructor (name, price, owner, houses, location){
+    //this.name = name
+   // this.price = price 
+    //this.owner = owner
+   // this.houses = houses 
+    //this.location = location 
 
 
 
 
+TotalRoll = 0
 PlayerRoll = 0; 
 activePlayer = 0; 
 
@@ -29,41 +51,22 @@ playerLocation.push(DiceRolled);
 
 let TotalRoll = playerLocation.reduce(
     (accumulator, currentValue) => accumulator + currentValue); 
+  
+
+//document.querySelector('#spaceCount-' + activePlayer).innerHTML = 
+//spaceNames[TotalRoll].textContent
+
+
+let remainder = TotalRoll % spaceNames.length
+if (TotalRoll > 39 ) {
     
-   
-document.querySelector('#spaceCount-' + activePlayer).innerHTML =TotalRoll;
-
-
-
-
-
+    console.log("the remainder is "+ remainder)}
+console.log("player has rolled a " + TotalRoll)
 
 }); 
 
 
 
-
-
-
-
-
-
-
-space = document.querySelectorAll('.space'); 
-
-
-
-
-
-
-
-
-const spacesCollection = document.querySelectorAll('.space');
-let spacesArr = Array.from(spacesCollection);
-
-spacesArr.forEach(function () {
-
-})
 
 
 
