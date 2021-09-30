9e48f3239cbfe029ca60d0c603e2028765085a60
unknown <Doodle23@yahoo.com>
2021-09-30 18:11:58 +0900

    added hotel prices to the property objects

--- monopoly.js
+++ monopoly.js
@@ -30,12 +30,13 @@ let spacenamesUnordered= Array.from(names);
     const spaceNames = index.map(i => spacenamesUnordered[i])
 
 
-console.log(spaceNames)
 
 
 
 
-const SpaceObjects = (price, name) => {
+// the house values here only handle the prices. Posession of the houses are stored on the player object 
+const SpaceObjects = (price, name, rent , colorsetrent, house1rent, house2rent, 
+  house3rent, house4rent, hotelrent) => {
     
     return { 
       price: price, 
@@ -43,47 +44,56 @@ const SpaceObjects = (price, name) => {
       hotels: 0, 
       owner: "The bank",
       name:name, 
+      rent: rent,
+      colorsetrent:colorsetrent, 
+      house1rent:house1rent, 
+      house2rent:house2rent, 
+      house3rent:house3rent, 
+      house4rent:house4rent, 
+      hotelrent:hotelrent, 
+      
     
     }
 
   };
 
   const go = SpaceObjects(200,"go")
-  const mediterennan = SpaceObjects(50, "mediternnean")
-  const communitychest = SpaceObjects(0,"Community Chest ")
-  const balticavenue = SpaceObjects(50, " Baltic Avenue ")
-  const incometax = SpaceObjects(-200,"Income Tax")
-  const readingRailRoad = SpaceObjects(200, "Reading Railroad ")
-  const orientalAvenue = SpaceObjects(100, " Oriental Avenue ")
-  const chance = SpaceObjects(0, " Chance ")
-  const vermont = SpaceObjects(100, " Vermont Avenue ")
-  const vonnecticut = SpaceObjects(120, " Conneticut Avenue ")
-  const justVisiting = SpaceObjects(0, " Just Visiting")
-  const stcharlesPlace = SpaceObjects(140, "St. Charles Place ")
+  const mediterennan = SpaceObjects(50, "mediternnean", 2, 4, 10, 30, 90, 160, 250)
+  const communitychest = SpaceObjects(0,"Community Chest ", 0,0,0,0,0,0,0)
+  const balticavenue = SpaceObjects(50, " Baltic Avenue " , 4, 8, 20, 60, 180, 320, 450)
+  const incometax = SpaceObjects(-200,"Income Tax",0,0,0,0,0,0,0)
+  const readingRailRoad = SpaceObjects(200, "Reading Railroad ", 25, 200, 25,50,100,200, 0)
+  const orientalAvenue = SpaceObjects(100, " Oriental Avenue ", 6, 12, 30,90, 270,400,550 )
+  const chance = SpaceObjects(0, " Chance ",0,0,0,0,0,0,0)
+  const vermont = SpaceObjects(100, " Vermont Avenue ", 6, 12, 30, 90, 270, 400, 550)
+  const vonnecticut = SpaceObjects(120, " Conneticut Avenue ",8, 16,40,100,300,450,600)
+  const justVisiting = SpaceObjects(0, " Just Visiting",0,0,0,0,0,0,0)
+  const stcharlesPlace = SpaceObjects(140, "St. Charles Place " , 10, 20, 50,150, 450, 625, 750 )
   const electricCompany = SpaceObjects(150, " Electric Company")
-  const statesavenue = SpaceObjects(140, " States Avenue ")
-  const virginia = SpaceObjects(160, " Virgina Avenue ")
-  const pennRR = SpaceObjects(200, " Penn Railroad")
-  const stJamesPlace = SpaceObjects(180, "St. James Place ")
-  const tenessee = SpaceObjects(180, " Tenessee Avenue")
-  const newYork = SpaceObjects(200, " New York Avenue")
-  const freeParking = SpaceObjects(0, " Free Parking")
-  const kentucky = SpaceObjects(220, " Kentucky Avenue ")
-  const indiana = SpaceObjects(220, " Indiana Avenue ")
-  const illinois = SpaceObjects (220, " Illinois Avenue")
-  const band0 = SpaceObjects(200, "B & O Railroad")
-  const atlantic = SpaceObjects(200," Atlantic Avenue ")
-  const ventur = SpaceObjects(200, " Ventur Avenue ")
+  const statesavenue = SpaceObjects(140, " States Avenue ", 10, 20, 50, 150, 450, 625, 750)
+  const virginia = SpaceObjects(160, " Virgina Avenue ", 12, 24, 60, 180, 500, 700, 900)
+  const pennRR = SpaceObjects(200, " Penn Railroad", 25, 200, 25,50,100,200, 0)
+  const stJamesPlace = SpaceObjects(180, "St. James Place ", 14, 28,70,200, 550,750,950)
+  const tenessee = SpaceObjects(180, " Tenessee Avenue", 14, 28, 70, 200,550,750, 950)
+  const newYork = SpaceObjects(200, " New York Avenue", 16, 32, 80, 220, 600, 800, 1000)
+  const freeParking = SpaceObjects(0, " Free Parking",0,0,0,0,0,0,0,)
+  const kentucky = SpaceObjects(220, " Kentucky Avenue ", 18, 36, 90,250, 700, 875, 1050 )
+  const indiana = SpaceObjects(220, " Indiana Avenue ", 18, 36, 90,250, 700, 875, 1050 )
+  const illinois = SpaceObjects (220, " Illinois Avenue",300, 20, 400, 100, 300, 750, 925, 1100)
+  const band0 = SpaceObjects(200, "B & O Railroad",25, 200, 25,50,100,200, 0)
+  const atlantic = SpaceObjects(200," Atlantic Avenue " ,22, 44, 110,330, 800, 975, 1150 )
+  const ventur = SpaceObjects(200, " Ventur Avenue " ,22, 44, 110,330, 800, 975, 1150)
   const waterworks = SpaceObjects(120, " Water Works ")
-  const marvinGardens = SpaceObjects(240, " Marvin Gardens")
-  const jail = SpaceObjects(0, "Jail")
-  const pacific = SpaceObjects(300, " Pacific Avenue ")
-  const northCarolina = SpaceObjects(300, " North Carolina")
-  const pennsylvania = SpaceObjects(320, "Pennsylvania ")
-  const shortLine = SpaceObjects(200, " Short Line RailRoad")
-  const parkPlace = SpaceObjects(350, " Park Place")
-  const luxuryTax = SpaceObjects(-100," Luxury Tax" )
-  const boardWalk = SpaceObjects(400, " Board Walk")
+  const marvinGardens = SpaceObjects(240, " Marvin Gardens" ,24, 48, 120, 360, 850, 1025, 1200)
+  const jail = SpaceObjects(0, "Jail",0,0,0,0,0,0,0)
+  const pacific = SpaceObjects(300, " Pacific Avenue " , 26, 52, 130, 390, 900, 1100, 1275)
+  const northCarolina = SpaceObjects(300, " North Carolina", 26, 52, 130, 390, 900, 1100, 1275)
+  const pennsylvania = SpaceObjects(320, "Pennsylvania ", 28, 56, 150, 450, 1000, 1200, 1400)
+  const shortLine = SpaceObjects(200, " Short Line RailRoad",25, 200, 25,50,100,200, 0)
+  const parkPlace = SpaceObjects(350, " Park Place", 35, 70, 175, 500, 1100, 1300, 1500)
+  const luxuryTax = SpaceObjects(-100," Luxury Tax",0,0,0,0,0,0,0 )
+  const boardWalk = SpaceObjects(400, " Board Walk", 50, 100, 200, 600, 1400, 1700, 2000)
+ 
  
   let SpacesArray = [go, mediterennan, communitychest, balticavenue, incometax, readingRailRoad, 
   orientalAvenue, chance, vermont, vonnecticut, justVisiting, stcharlesPlace, electricCompany, statesavenue, virginia, 
@@ -92,7 +102,7 @@ const SpaceObjects = (price, name) => {
   luxuryTax, boardWalk]
 
 
-
+console.log(SpacesArray)
 
   const Players = (name, cash, property, items, ) => {
 
@@ -163,18 +173,23 @@ const SpaceObjects = (price, name) => {
       
         if (landedOn.owner != "The bank"){
 
-        usermessage[0].textContent = " Sorry, but you have landed on " + landedOn.name + 
+        
+          
+          usermessage[0].textContent = " Sorry, but you have landed on " + landedOn.name + 
         ". It is currently owned by  " + landedOn.owner + 
         "'s property. The rent is " + landedOn.price + "? "
-        }
+        
+
+      
+      }
  
         
+
+
         let playerMoney = document.getElementsByClassName("cash")[0];
         playerMoney.textContent =  " Your cash balance is " + this.cash
        
-        
-   
-     
+    
            
       document.getElementsByClassName("modal")[0].style.display = "block"
       
@@ -182,7 +197,7 @@ const SpaceObjects = (price, name) => {
       chanceCardmessage.style.display = " none"
 
       
-      /*if (landedOn.name === chance.name){
+      if (landedOn.name === chance.name){
       chanceCardmessage.style.display = "block"
       chanceCardmessage.innerHTML = " Go directly to jail. Do not pass go and do not collect $200" 
       TotalRoll = 10; 
@@ -203,12 +218,9 @@ const SpaceObjects = (price, name) => {
 
     }
       
-     */
+   
       
-    console.log(DiceRolled +" dice rolled")
-    console.log ( TotalRoll + "total roll")
-    console.log(this.location + " this location is ")
-        
+  
     buttonYes.addEventListener('click', function(){
 
           
@@ -272,8 +284,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     player2.moveplayer() 
   }
 
-  console.log(player1)
-  console.log(player2)
+  
 })
 
 
